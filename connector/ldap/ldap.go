b5ec820dd7b2ebbce2f5367170a8d7ab53179261
Nandor Kracser <bonifaido@gmail.com>
2019-10-11 15:22:22 +0200

    ldap: if set, add PreferredUsernameAttrAttr so search
    Signed-off-by: Nandor Kracser <bonifaido@gmail.com>

--- connector/ldap/ldap.go
+++ connector/ldap/ldap.go
@@ -389,6 +389,10 @@ func (c *ldapConnector) userEntry(conn *ldap.Conn, username string) (user ldap.E
 		req.Attributes = append(req.Attributes, c.UserSearch.NameAttr)
 	}
 
+	if c.UserSearch.PreferredUsernameAttrAttr != "" {
+		req.Attributes = append(req.Attributes, c.UserSearch.PreferredUsernameAttrAttr)
+	}
+
 	c.logger.Infof("performing ldap search %s %s %s",
 		req.BaseDN, scopeString(req.Scope), req.Filter)
 	resp, err := conn.Search(req)
